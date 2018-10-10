822fa14eb1de267f90747131759f3fc6402b3ab7
Edward Huang <26125482@qq.com>
2018-10-10 23:32:22 +0800

    ausu 20181010

--- Test/TryTensentCloud.py
+++ Test/TryTensentCloud.py
@@ -2,20 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import pymysql
-import time
-import tencentcloud as QQ
 
-QQ.__version__
+# help(pymysql.connect)
 
-# def connect_db():
-#     """Connect database and return db and cursor"""
-#     db = pymysql.connect(host="cdb-86podtot.gz.tencentcdb.com:10016",user='root',
-#                          passwd='secret227@@&',db="cdb210819")
-#     cursor = db.cursor()
-#     return (db,cursor)
-#
-# db,cursor = connect_db()
-# cursor.execute("SELECT VERSION()")
-# data = cursor.fetchone()
-# print("Database version : %s " % data)
-# # db.close()
+def connect_db():
+    """Connect database and return db and cursor"""
+    db = pymysql.connect(host="cdb-91rtu6jl.gz.tencentcdb.com",port=10045,user='root',
+                         passwd='3621@(!jj',db="Stock_test")
+    cursor = db.cursor()
+    return (db,cursor)
+
+db,cursor = connect_db()
+cursor.execute("SELECT VERSION()")
+data = cursor.fetchone()
+print("Database version : %s " % data)
+db.close()
