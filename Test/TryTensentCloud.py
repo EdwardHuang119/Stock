ed27fc4b849920e87518c9ee19677e514b4530d0
Edward Huang <26125482@qq.com>
2018-10-11 17:21:32 +0800

    From MAc

--- Test/TryTensentCloud.py
+++ Test/TryTensentCloud.py
@@ -2,18 +2,48 @@
 # -*- coding: utf-8 -*-
 
 import pymysql
+import os
+from configparser import ConfigParser
 
 # help(pymysql.connect)
 
-def connect_db():
-    """Connect database and return db and cursor"""
-    db = pymysql.connect(host="cdb-91rtu6jl.gz.tencentcdb.com",port=10045,user='root',
-                         passwd='3621@(!jj',db="Stock_test")
-    cursor = db.cursor()
-    return (db,cursor)
+# fatherpath = os.getcwd()
+configname = 'config.conf'
+fatherpath = os.path.abspath(os.path.dirname(os.getcwd()))
+configpath = fatherpath+'confing'+'//'+configname
+cf = ConfigParser()
+cf.read(configpath)
 
-db,cursor = connect_db()
-cursor.execute("SELECT VERSION()")
-data = cursor.fetchone()
-print("Database version : %s " % data)
-db.close()
+a = cf.read()
+
+# host = cf.get("tencentcdb","host")
+# /Users/Mac/PycharmProjects/Stock/confing
+
+print(a)
+
+
+# def connect_db():
+#     """Connect database and return db and cursor"""
+#     db = pymysql.connect(host="cdb-91rtu6jl.gz.tencentcdb.com",port=10045,user='root',
+#                          passwd='3621@(!jj',db="Stock_test")
+#     cursor = db.cursor()
+#     return (db,cursor)
+#
+# db,cursor = connect_db()
+# cursor.execute("SELECT VERSION()")
+# data = cursor.fetchone()
+# print("Database version : %s " % data)
+# db.close()
+
+# def connect_db():
+#     """Connect database and return db and cursor"""
+#     db = pymysql.connect(host="cdb-91rtu6jl.gz.tencentcdb.com",port=10045,user='root',
+#                          passwd='3621@(!jj',db="Stock_test")
+#     cursor = db.cursor()
+#     return (db,cursor)
+#
+# db,cursor = connect_db()
+# cursor.execute("SELECT VERSION()")
+# data = cursor.fetchone()
+# print("Database version : %s " % data)
+# db.close()
