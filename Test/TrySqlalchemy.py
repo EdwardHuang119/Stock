22bcced994d6bc4414cad255a65ecd98356ebe30
Edward Huang <26125482@qq.com>
2018-10-19 07:16:17 +0800

    ausu 20181019

--- Test/TrySqlalchemy.py
+++ Test/TrySqlalchemy.py
@@ -4,14 +4,14 @@ from sqlalchemy import create_engine, event,MetaData,Table
 # from sqlalchemy import *
 import pymysql
 
-engine = create_engine('mysql+pymysql://root:3621@(!@cdb-91rtu6jl.gz.tencentcdb.com:10045/Stock_test?charset=utf8')
+engine = create_engine('mysql+pymysql://root:4C6V&g96X@cdb-91rtu6jl.gz.tencentcdb.com:10045/Stock_test?charset=utf8')
 conn = engine.connect()
-# metadata = MetaData(engine)
+metadata = MetaData(engine)
 
-# def serch():
-#     table = Table('Stock_basic', metadata, autoload=True)
-#     s = select([table])
-#     conn.execute(s)
+def serch():
+    table = Table('Stock_basic', metadata, autoload=True)
+    s = select([table])
+    conn.execute(s)
 
 
 
